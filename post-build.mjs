a6e63675b8cd2f65603b19da58005f61492c00f0
Sergey Volkov <js2me@outlook.com>
2024-11-04 12:41:15 +0300

    internal: improve publish script

--- post-build.mjs
+++ post-build.mjs
@@ -1,8 +1,22 @@
-import { postBuildScript } from 'js2me-exports-post-build-script';
+import { postBuildScript, publishScript } from './dist/index.js';
 
 postBuildScript({
   buildDir: 'dist',
   rootDir: '.',
   srcDirName: 'src',
   filesToCopy: ['LICENSE', 'README.md'],
+  updateVersion: process.env.PUBLISH_VERSION,
+  onPackageVersionChanged: (nextVersion, currVersion) => {
+    if (process.env.PUBLISH) {
+      publishScript({
+        nextVersion,
+        currVersion,
+        publishCommand: 'pnpm publish',
+        commitAllCurrentChanges: true,
+        createTag: true,
+        githubRepoLink: 'https://github.com/js2me/mobx-location-history',
+        cleanupCommand: 'pnpm clean',  
+      });
+    }
+  }
 });
