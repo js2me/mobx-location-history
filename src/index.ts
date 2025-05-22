07f0745e2a92fe78a371d8e3c13e9de40e73b925
Sergey Volkov <js2me@outlook.com>
2025-05-22 23:57:52 +0300

    feat: add AnyHistory type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+import { IHistory } from './history/index.js';
+import { IMemoryHistory } from './memory-history/index.js';
+
 export * from './history/index.js';
 export * from './location/index.js';
 export * from './query-params/index.js';
 export * from './query-param/index.js';
 export * from './memory-history/index.js';
+
+export type AnyHistory = IMemoryHistory | IHistory;
