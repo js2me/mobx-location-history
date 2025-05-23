9739792963efd85619732e1d0d9a0c7b286c9f46
Sergey Volkov <js2me@outlook.com>
2025-05-23 17:57:46 +0300

    docs: add docs for histories

--- src/history/index.ts
+++ src/history/index.ts
@@ -15,6 +15,9 @@ import { AnyObject } from 'yummies/utils/types';
 export * from 'history';
 
 export type ObservableHistory<THistory extends History> = THistory & {
+  /**
+   * Unsubscribe from original history.listen
+   */
   destroy: VoidFunction;
 };
 
@@ -57,18 +60,54 @@ const makeHistoryObservable = <THistory extends History>(
   });
 };
 
+/**
+ * Browser history stores the location in regular URLs. This is the standard for
+ * most web apps, but it requires some configuration on the server to ensure you
+ * serve the same app at multiple URLs.
+ *
+ * @see https://github.com/remix-run/history/tree/main/docs/api-reference.md#createbrowserhistory
+ *
+ * MobX enhancers:
+ * - `observable.deep` `history.location`
+ * - `observable.ref` `history.action`
+ */
 export const createBrowserHistory = (
   options?: WithObservableHistoryParams<BrowserHistoryOptions>,
 ) => makeHistoryObservable(createBrowserHistoryLib(options), options);
 
+/**
+ * Hash history stores the location in window.location.hash. This makes it ideal
+ * for situations where you don't want to send the location to the server for
+ * some reason, either because you do cannot configure it or the URL space is
+ * reserved for something else.
+ *
+ * @see https://github.com/remix-run/history/tree/main/docs/api-reference.md#createhashhistory
+ *
+ * MobX enhancers:
+ * - `observable.deep` `history.location`
+ * - `observable.ref` `history.action`
+ */
 export const createHashHistory = (
   options?: WithObservableHistoryParams<HashHistoryOptions>,
 ) => makeHistoryObservable(createHashHistoryLib(options), options);
 
+/**
+ * Memory history stores the current location in memory. It is designed for use
+ * in stateful non-browser environments like tests and React Native.
+ *
+ * @see https://github.com/remix-run/history/tree/main/docs/api-reference.md#creatememoryhistory
+ *
+ * MobX enhancers:
+ * - `observable.deep` `history.location`
+ * - `observable.ref` `history.action`
+ */
 export const createMemoryHistory = (
   options?: WithObservableHistoryParams<MemoryHistoryOptions>,
 ) => makeHistoryObservable(createMemoryHistoryLib(options), options);
 
+/**
+ * Checks if history object is created using this library
+ */
 export const isObservableHistory = <THistory extends History>(
   history: THistory,
 ): history is ObservableHistory<THistory> => 'destroy' in history;
