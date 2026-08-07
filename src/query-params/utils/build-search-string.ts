b580ae352080953d27d21612a85a50c232b2d4ca
Sergey Volkov <js2me@outlook.com>
2026-08-07 09:53:43 +0300

    feat: optimize build ss parse ss

--- src/query-params/utils/build-search-string.ts
+++ src/query-params/utils/build-search-string.ts
@@ -1,5 +1,11 @@
 import { stringify } from 'qs';
 
+const defaultBuildOptions = {
+  addQueryPrefix: true,
+  skipNulls: true,
+  arrayFormat: 'comma',
+} as const;
+
 /**
  * Converts an object into a URL search string.
  * Filters out entries with null or undefined values.
@@ -10,10 +16,8 @@ export const buildSearchString = (
   data: Record<string, any>,
   options?: Parameters<typeof stringify>[1],
 ) => {
-  return stringify(data, {
-    addQueryPrefix: true,
-    skipNulls: true,
-    arrayFormat: 'comma',
-    ...options,
-  });
+  return stringify(
+    data,
+    options ? { ...defaultBuildOptions, ...options } : defaultBuildOptions,
+  );
 };
--- src/query-params/utils/parse-search-string.ts
+++ src/query-params/utils/parse-search-string.ts
@@ -2,6 +2,8 @@ import { parse } from 'qs';
 
 export type ParsedSearchString = Record<string, string>;
 
+const defaultParseOptions = { ignoreQueryPrefix: true } as const;
+
 /**
  * Converts a URL search string into an object.
  *
@@ -11,5 +13,8 @@ export const parseSearchString = <TResult = ParsedSearchString>(
   search: string,
   options?: Parameters<typeof parse>[1],
 ): TResult => {
-  return parse(search, { ignoreQueryPrefix: true, ...options }) as TResult;
+  return parse(
+    search,
+    options ? { ...defaultParseOptions, ...options } : defaultParseOptions,
+  ) as TResult;
 };
